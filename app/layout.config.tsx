da57855fe4a654b83442416d811925250414d7b3
Nikita <github@nikiv.dev>
2025-10-17 09:04:34 +0300

    .

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -8,18 +8,18 @@ import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared"
  * Docs Layout: app/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
-  githubUrl: "https://github.com/nikitavoloboev/nikiv",
+  githubUrl: "https://github.com/nikivdev/nikiv",
   nav: {
     title: "nikiv.dev",
   },
   links: [
     {
       text: "GitHub",
-      url: "https://github.com/nikitavoloboev",
+      url: "https://github.com/nikivdev",
     },
     {
       text: "X",
-      url: "https://x.com/nikitavoloboev",
+      url: "https://x.com/nikivdev",
     },
   ],
 }
--- readme.md
+++ readme.md
@@ -18,4 +18,4 @@ Any PR to improve is welcome. [codex](https://github.com/openai/codex) & [cursor
 
 ### 🖤
 
-[![Discord](https://go.nikiv.dev/badge-discord)](https://go.nikiv.dev/discord) [![X](https://go.nikiv.dev/badge-x)](https://x.com/nikitavoloboev) [![nikiv.dev](https://go.nikiv.dev/badge-nikiv)](https://nikiv.dev)
+[![Discord](https://go.nikiv.dev/badge-discord)](https://go.nikiv.dev/discord) [![X](https://go.nikiv.dev/badge-x)](https://x.com/nikivdev) [![nikiv.dev](https://go.nikiv.dev/badge-nikiv)](https://nikiv.dev)
